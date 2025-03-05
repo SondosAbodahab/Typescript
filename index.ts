aa79f2b18836a2c9a4c59a5809bcd45428519c29
SondosAbodahab <sondos.ahmed031@gmail.com>
2025-03-05 21:31:57 +0200

    feat: :sparkles: axios & first interface

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
 const URL = 'https://jsonplaceholder.typicode.com/todos/1'
+interface Todo {
+     id: number,
+     title:string,
+     completed: boolean
+}
 
 axios.get(URL).then( res =>{
-     const todo = res.data;
-     const Id = todo.id;
-     const title = todo.title;
-     const finished = todo.completed;
-     console.log(finished)
+     const todo:Todo = res.data;
+     logTodo(todo)
 })
+
+ const logTodo = (todo:Todo)=>{
+     console.log(`
+          The TODO with ID ${todo.id},
+          has a title of ${todo.title},
+          Is it finished? ${todo.completed}
+          `)
+ }
