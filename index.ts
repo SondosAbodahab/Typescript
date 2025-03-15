6695239778637a5c0e67c76ce6fcb4dd590782e7
SondosAbodahab <sondos.ahmed031@gmail.com>
2025-03-15 02:51:56 +0200

    feat: :sparkles: google maps integration

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-import { User } from './src/User';
-import { Company } from './src/Company';
+/// <reference types="@types/google.maps" />
+import { User } from "./src/User";
+import { Company } from "./src/Company";
+import { CustomMap } from './src/CustomMap';
 
-const user = new User()
+const user = new User();
 console.log(user);
 
-const company = new Company()
+const company = new Company();
 console.log(company);
 
+
+const customMap =new CustomMap("map")
+customMap.addMarker(user)
+customMap.addMarker(company)
