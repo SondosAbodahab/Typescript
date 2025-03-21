c7fee812b0cdcae608474c41ef5cb346c78dfcbf
SondosAbodahab <sondos.ahmed031@gmail.com>
2025-03-22 01:34:53 +0200

    fix: :bug: convert to enum

--- stats-from-excel-sheet/src/index.ts
+++ stats-from-excel-sheet/src/index.ts
@@ -1,9 +1,28 @@
-import fs from 'fs';
+import fs from "fs";
 
-const matches = fs.readFileSync('./football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map((row:string):string[]=>{
-    return row.split(',')
-})
+const matches = fs
+  .readFileSync("./football.csv", {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
+  
+enum MatchResult {
+    HomeWin = "H",
+     AwayWin = "A",
+     Draw= "D"
+}
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin ) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+console.log(manUnitedWins);
+
