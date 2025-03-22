634bd9a56f167b79fb2bb7163495a967d60ebd11
SondosAbodahab <sondos.ahmed031@gmail.com>
2025-03-23 00:46:53 +0200

    refactor: :recycle: composition approach

--- stats-from-excel-sheet/src/CsvFilleReader.back.ts
+++ stats-from-excel-sheet/src/CsvFilleReader.back.ts
@@ -3,7 +3,7 @@ import { dateStringToDate } from "./utils";
 import { MatchResult } from "./MatchResult";
 export type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-export abstract class CsvFileReader {
+export class CsvFileReader {
   data: MatchData[] = [];
   constructor(public fileName: string) {}
   read(): void {
--- stats-from-excel-sheet/src/index.ts
+++ stats-from-excel-sheet/src/index.ts
@@ -1,19 +1,19 @@
-import { MatchReader } from "./inheritance/MatchReader";
+import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader('football.csv')
-reader.read()
+const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
-const dateOfFirstMatch =reader.data[0][0]
-console.log(dateOfFirstMatch)
+
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin ) {
+for (let match of matchReader.matches) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
 console.log(manUnitedWins);
-
