6428f698c2a71ec00296286e3c278508078f462c
SondosAbodahab <sondos.ahmed031@gmail.com>
2025-03-24 22:40:56 +0200

    feat: :sparkles: use json server to store user events

--- web-framework/src/User.ts
+++ web-framework/src/User.ts
@@ -24,4 +24,17 @@ export class User {
     handlers.push(callback)
     this.events[eventName] = handlers
   }
+  // triggering the required event
+  trigger(eventName:string):void{
+    const handlers = this.events[eventName];
+    if(!handlers || handlers.length === 0){
+      return;
+    }
+    handlers.forEach(callback=>{
+      callback()
+    })
+
+  }
+
+
 }
--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,14 +1,15 @@
 import { User } from "./User";
 
-
 const user = new User({ name: "ahmed", age: 30 });
 
-
-
-user.on('change' , ()=>{
-    console.log('ana changed')
+user.on("change", () => {
+  console.log("ana changed");
+});
+user.on("change", () => {
+  console.log("ana changed #2");
+});
+user.on('lol', ()=>{
+    console.log('ana lol');
 })
-user.on('lol' , ()=>{ console.log('ana changed')
-})
-
+user.trigger('change')
 console.log(user);
